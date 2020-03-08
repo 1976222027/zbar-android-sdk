d0cbdcfab626a28a880fff1792b320605e42a569
1976222027 <2010MA520ma>
2020-03-08 13:51:05 +0800

    自编译zbar扫码SDK

---
+++ camera/src/main/java/com/mahongyin/zbar/camera/CameraManager.java
@@ -0,0 +1,176 @@
+package com.mahongyin.zbar.camera;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import java.io.IOException;
+
+/**
+ * <p>Camera manager.</p>
+ * Created by Yan Zhenjie on 2017/5/10.
+ */
+public final class CameraManager {
+
+    private final CameraConfiguration mConfiguration;
+
+    private static Camera mCamera;// ADD: 2019/2/13 新添加:增加static
+
+    public CameraManager(Context context) {
+        this.mConfiguration = new CameraConfiguration(context);
+    }
+
+    /**
+     * Opens the mCamera driver and initializes the hardware parameters.
+     *
+     * @throws Exception ICamera open failed, occupied or abnormal.
+     */
+    public synchronized void openDriver() throws Exception {
+        if (mCamera != null) return;
+
+        mCamera = Camera.open();
+        if (mCamera == null) throw new IOException("The camera is occupied.");
+
+        mConfiguration.initFromCameraParameters(mCamera);
+
+        Camera.Parameters parameters = mCamera.getParameters();
+        String parametersFlattened = parameters == null ? null : parameters.flatten();
+        try {
+            mConfiguration.setDesiredCameraParameters(mCamera, false);
+        } catch (RuntimeException re) {
+            if (parametersFlattened != null) {
+                parameters = mCamera.getParameters();
+                parameters.unflatten(parametersFlattened);
+                try {
+                    mCamera.setParameters(parameters);
+                    mConfiguration.setDesiredCameraParameters(mCamera, true);
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Closes the camera driver if still in use.
+     */
+    public synchronized void closeDriver() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    /**
+     * Camera is opened.
+     *
+     * @return true, other wise false.
+     */
+    public boolean isOpen() {
+        return mCamera != null;
+    }
+
+    /**
+     * Get camera configuration.
+     *
+     * @return {@link CameraConfiguration}.
+     */
+    public CameraConfiguration getConfiguration() {
+        return mConfiguration;
+    }
+
+    /**
+     * Camera start preview.
+     *
+     * @param holder          {@link SurfaceHolder}.
+     * @param previewCallback {@link Camera.PreviewCallback}.
+     * @throws IOException if the method fails (for example, if the surface is unavailable or unsuitable).
+     */
+    public void startPreview(SurfaceHolder holder, Camera.PreviewCallback previewCallback) throws IOException {
+        if (mCamera != null) {
+            mCamera.setDisplayOrientation(90);
+            mCamera.setPreviewDisplay(holder);
+            mCamera.setPreviewCallback(previewCallback);
+            mCamera.startPreview();
+        }
+    }
+
+    /**
+     * Camera stop preview.
+     */
+    public void stopPreview() {
+        if (mCamera != null) {
+            try {
+                mCamera.stopPreview();
+            } catch (Exception ignored) {
+                // nothing.
+            }
+            try {
+                mCamera.setPreviewDisplay(null);
+            } catch (IOException ignored) {
+                // nothing.
+            }
+        }
+    }
+
+    /**
+     * Focus on, make a scan action.
+     *
+     * @param callback {@link Camera.AutoFocusCallback}.
+     */
+    public void autoFocus(Camera.AutoFocusCallback callback) {
+        if (mCamera != null)
+            try {
+                mCamera.autoFocus(callback);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+
+    /**
+     * 获取相机的方法 //调用闪光灯需要和扫码同一camera
+     * @return mCamera
+     */
+    public static Camera getCamera(){
+        return mCamera;
+    }
+    /*
+     *闪光灯调开关用完 在你的Activity 的onDestroy()方法里记得 加上 mCamera.release();【页面销毁时关闭闪光灯】
+     */
+    
+    /**
+     * 打开闪光灯  //开启camera闪光灯
+     */
+    public void openFlashLight() {
+
+            if (mCamera == null) {
+                return;
+            }
+//             获取对象 getCamera();
+           // mCamera.startPreview();//扫码已开启 单纯闪光灯 不要这句
+            Camera.Parameters parameter = mCamera.getParameters();
+            parameter.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            mCamera.setParameters(parameter);
+        }
+
+    /**
+     * 关闭闪光灯//关闭camera闪光灯
+     */
+    public void closeFlashLight() {
+
+            if (mCamera == null) {
+                return;
+            }
+            Camera.Parameters parameter = mCamera.getParameters();
+            parameter.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            mCamera.setParameters(parameter);
+
+//        mCamera.stopPreview();//扫码还要用camera 单纯闪光灯 不要这句
+//        mCamera = null;
+
+//        mCamera.release();//TODO 调用时在页面销毁onDestroy()里再加这1句
+
+    }
+
+}
---
+++ sample/src/main/java/com/mahongyin/zbar/sample/QRScanActivity.java
@@ -0,0 +1,232 @@
+package com.mahongyin.zbar.sample;
+
+import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+
+import com.mahongyin.zbar.camera.CameraManager;
+import com.mahongyin.zbar.camera.CameraPreview;
+import com.mahongyin.zbar.camera.ScanCallback;
+import com.mahongyin.zbar.sample.R;
+
+/**
+ * <p>Scan UI.</p>
+ * Created by Yan Zhenjie on 2017/5/10.
+ */
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class QRScanActivity extends AppCompatActivity {
+    public static final int REQUEST_CAMERA = 120; //相机请求码
+    private RelativeLayout mScanCropView;
+    private ImageView mScanLine;
+    private ValueAnimator mScanAnimator;
+    private CameraPreview mPreviewView;
+    Context context;
+    Camera camera;//zbar 已经初始化了吧
+    //    CameraManager mCameraManager;
+    //FlashUtils utils;
+    Switch flashControl;
+    String mCameraId;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scan);
+        context = this;
+//        checkReadPermission(Manifest.permission.CAMERA, REQUEST_CAMERA);
+        mPreviewView = (CameraPreview) findViewById(R.id.capture_preview);
+        mScanCropView = (RelativeLayout) findViewById(R.id.capture_crop_view);
+        mScanLine = (ImageView) findViewById(R.id.capture_scan_line);
+        mPreviewView.setScanCallback(resultCallback);
+//        mCameraManager = new CameraManager(context);
+        //utils = new FlashUtils(this);
+
+        flashControl = findViewById(R.id.flash_control);
+        flashControl.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {//打开
+                // utils.open();
+                // openFlashLight();
+                try {
+                    // camera = Camera.open();
+//                    camera= mCameraManager.getCamera();
+                    camera = CameraManager.getCamera();
+                    Camera.Parameters mParameters;
+                    mParameters = camera.getParameters();
+                    mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(mParameters);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            } else {
+                // closeFlashLight();
+                // utils.close();
+                try {
+                    Camera.Parameters mParameters;
+                    mParameters = camera.getParameters();
+                    mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                    camera.setParameters(mParameters);
+                    //camera.release();//扫码还要用 销毁时再re
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
+    /**
+     * 打开闪光灯
+     */
+    public void openFlashLight() {
+
+        if (camera == null) {
+            return;
+        }
+        camera = CameraManager.getCamera();
+        Camera.Parameters parameter = camera.getParameters();
+        parameter.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(parameter);
+    }
+
+    /**
+     * 关闭闪光灯
+     */
+    public void closeFlashLight() {
+        if (camera == null) {
+            return;
+        }
+        Camera.Parameters parameter = camera.getParameters();
+        parameter.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(parameter);//camera.getCamera()
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (camera != null) {
+//            camera.setPreviewCallback(null);
+//            camera.stopPreview();
+            camera.release();
+            camera = null;
+            //回收camera
+            //页面销毁时关闭灯光
+        }
+    }
+
+    /**
+     * Accept scan result.
+     */
+    private ScanCallback resultCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(String result) {
+            stopScan();
+            //  Toast.makeText(QRScanActivity.this, "扫描内容："+result, Toast.LENGTH_LONG).show();
+            Intent resultIntent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putString("result", result);
+            // bundle.putParcelable("bitmap", barcode);
+            resultIntent.putExtras(bundle);
+            setResult(RESULT_OK, resultIntent);
+            finish();
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mScanAnimator != null) {
+            checkReadPermission(Manifest.permission.CAMERA, REQUEST_CAMERA);//启动时就open camera了
+        }
+    }
+
+    @Override
+    public void onPause() {
+        // Must be called here, otherwise the camera should not be released properly.
+        stopScan();
+        super.onPause();
+    }
+
+    /**
+     * 判断是否有某项权限
+     *
+     * @param string_permission 权限
+     * @param request_code      请求码
+     */
+    public boolean checkReadPermission(String string_permission, int request_code) {//通过请求码判断
+        boolean flag = false;
+        //M 以下版本是不是直接就是已有权限？
+        if (ContextCompat.checkSelfPermission(this, string_permission) == PackageManager.PERMISSION_GRANTED) {//已有权限
+            flag = true;
+            startScanWithPermission();//有权就go
+        } else {//没权限 则申请权限
+            ActivityCompat.requestPermissions(this, new String[]{string_permission}, request_code);
+        }
+        return flag;
+    }
+
+    /**
+     * There is a camera when the direct scan.
+     */
+    private void startScanWithPermission() {
+        if (mPreviewView.start()) {
+            mScanAnimator.start();
+            //进行扫描扫完后 Toast 该finish 回传值
+        } else {//相机被占用
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.camera_failure)
+                    .setMessage(R.string.camera_hint)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+        }
+    }
+
+    /**
+     * Stop scan.
+     */
+    private void stopScan() {
+        mScanAnimator.cancel();
+        mPreviewView.stop();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (mScanAnimator == null) {
+            int height = mScanCropView.getMeasuredHeight() - 25;
+            mScanAnimator = ObjectAnimator.ofFloat(mScanLine, "translationY", 0F, height).setDuration(3000);
+            mScanAnimator.setInterpolator(new LinearInterpolator());
+            mScanAnimator.setRepeatCount(ValueAnimator.INFINITE);
+            mScanAnimator.setRepeatMode(ValueAnimator.REVERSE);
+            checkReadPermission(Manifest.permission.CAMERA, REQUEST_CAMERA);
+        }
+    }
+}
+
